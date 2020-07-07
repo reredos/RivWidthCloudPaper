a93a707a319e1fb63c5212506db27407f9f1619f
Austin Raney <aaraney@crimson.ua.edu>
2020-07-07 09:57:07 -0400

    Refactor python api into classes and functions. Batch now uses multiprocessing instead of calling with

--- RivWidthCloud_Python/rwc_landsat_batch.py
+++ RivWidthCloud_Python/rwc_landsat_batch.py
@@ -1,82 +1,167 @@
-if __name__ == '__main__':
+import ee
+import numpy as np
+import pandas as pd
+import getopt
+import os
+from os import listdir
+import pathlib
+import argparse
+from functools import partial
+from typing  import Union
 
-    # import ee
-    import numpy as np
-    import pandas as pd
-    import getopt
-    import os
-    from os import listdir
-    import argparse
-    from functions_batch import maximum_no_of_tasks
+from functions_batch import maximum_no_of_tasks
+from rwc_landsat_one_image import Rwc
+
+from multiprocessing import Pool
+import concurrent.futures as cf
+import asyncio
 
 
-    parser = argparse.ArgumentParser(prog = 'rwc_landsat_batch.py', description = "Batch execute rwc_landsat.py for a csv files that contains Landsat image IDs and/or point locations.\
-    (Example: python rwc_landsat_batch.py example_batch_input/example_batch_input.csv)")
-    parser.add_argument('ID_FILE', help = 'Csv file contains at least one column (named "LANDSAT_ID")', type = str)
-    parser.add_argument('-f', '--FORMAT', help = "Output file format ('csv' or 'shp'). Default: 'csv'", type = str, default = 'csv')
-    parser.add_argument('-w', '--WATER_METHOD', help = "Water classification method ('Jones2019' or 'Zou2018'). Default: 'Jones2019'", type = str, default = 'Jones2019')
-    parser.add_argument('-d', '--MAXDISTANCE', help = 'Default: 4000 meters', type = float, default = 4000)
-    parser.add_argument('-i', '--FILL_SIZE', help = 'Default: 333 pixels', type = float, default = 333)
-    parser.add_argument('-b', '--MAXDISTANCE_BRANCH_REMOVAL', help = 'Default: 500 pixels', type = float, default = 500)
-    parser.add_argument('-o', '--OUTPUT_FOLDER', help = 'Any existing folder name in Google Drive. Default: root of Google Drive', type = str, default = '')
-    parser.add_argument('-m', '--MAXIMUM_NO_OF_TASKS', help = 'Maximum number of tasks running simutaneously on the server. Default: 6', type = int, default = 6)
-    parser.add_argument('-s', '--START_NO', help = '(Re)starting task No. Helpful when restarting an interrupted batch processing. Default: 0 (start from the beginning)', type = int, default = 0)
+def _cli() -> dict:
+    parser = argparse.ArgumentParser(
+        prog="rwc_landsat_batch.py",
+        description="Batch execute rwc_landsat.py for a csv files that contains Landsat image IDs and/or point locations.\
+    (Example: python rwc_landsat_batch.py example_batch_input/example_batch_input.csv)",
+    )
+    parser.add_argument(
+        "landsat_id_file",
+        help='Csv file contains at least one column (named "landsat_id")',
+        type=pathlib.Path,
+    )
+    parser.add_argument(
+        "-f",
+        "--file_format",
+        help="Output file format ('csv' or 'shp'). Default: 'csv'",
+        type=str,
+        default="csv",
+    )
+    parser.add_argument(
+        "-w",
+        "--water_method",
+        help="Water classification method ('Jones2019' or 'Zou2018'). Default: 'Jones2019'",
+        type=str,
+        default="Jones2019",
+    )
+    parser.add_argument(
+        "-d", "--max_distance", help="Default: 4000 meters", type=float, default=4000
+    )
+    parser.add_argument(
+        "-i", "--fill_size", help="Default: 333 pixels", type=float, default=333
+    )
+    parser.add_argument(
+        "-b",
+        "--max_distance_branch_removal",
+        help="default: 500 pixels",
+        type=float,
+        default=500,
+    )
+    parser.add_argument(
+        "-o",
+        "--output_folder",
+        help="any existing folder name in google drive. default: root of google Drive",
+        type=str,
+        default="",
+    )
+    parser.add_argument(
+        "-m",
+        "--maximum_number_of_tasks",
+        help="Maximum number of tasks running simutaneously on the server. Default: 10",
+        type=int,
+        default=10,
+    )
+    parser.add_argument(
+        "-s",
+        "--start_number",
+        help="(Re)starting task No. Helpful when restarting an interrupted batch processing. Default: 0 (start from the beginning)",
+        type=int,
+        default=0,
+    )
 
-    group_validation = parser.add_argument_group(title = 'Batch run the RivWidthCloud in POINT mode',
-    description = 'In POINT mode, the csv file needs to have addtional columns named "Point_ID", "Longitude", and "Latitude"\
+    group_validation = parser.add_argument_group(
+        title="Batch run the RivWidthCloud in POINT mode",
+        description='In POINT mode, the csv file needs to have addtional columns named "Point_ID", "Longitude", and "Latitude"\
     The point must locate within the bounds of the scene. \
-    (Example: python rwc_landsat_batch.py example_batch_input/example_batch_input.csv -p)')
+    (Example: python rwc_landsat_batch.py example_batch_input/example_batch_input.csv -p)',
+    )
 
-    group_validation.add_argument('-p', '--POINT', help = 'Enable the POINT mode', action = 'store_true')
-    group_validation.add_argument('-r', '--BUFFER', help = 'Radius of the buffered region around the point location', type = float, default = 4000)
+    group_validation.add_argument(
+        "-p", "--point_mode", help="Enable the POINT mode", action="store_true"
+    )
+    group_validation.add_argument(
+        "-r",
+        "--radius",
+        help="Radius of the buffered region around the point location",
+        type=float,
+        default=4000,
+    )
 
-    args = parser.parse_args()
+    return vars(parser.parse_args())
 
-    ID_FILE = args.ID_FILE
-    FORMAT = args.FORMAT
-    WATER_METHOD = args.WATER_METHOD
-    MAXDISTANCE = args.MAXDISTANCE
-    FILL_SIZE = args.FILL_SIZE
-    MAXDISTANCE_BRANCH_REMOVAL = args.MAXDISTANCE_BRANCH_REMOVAL
-    OUTPUT_FOLDER = args.OUTPUT_FOLDER
-    MAXIMUM_NO_OF_TASKS= args.MAXIMUM_NO_OF_TASKS
-    START_NO = args.START_NO
+def _caller(*args, func=None) ->  partial:
+    if len(args) > 1:
+        landsat_id, lat, lon = args
+        return partial(func,  landsat_id=landsat_id, lat=lat, lon=lon)()
+    else:
+        return partial(func, landsat_id=args[0])()
+    # p()
+    # print(f"runnning on {os.pid()}")
 
-    POINTMODE = args.POINT
-    RADIUS = args.BUFFER
+def rwc_landsat_batch(
+    landsat_id_file: str = None,
+    lon: Union[float, None] = None,
+    lat: Union[float, None] = None,
+    max_distance: float = 4000,
+    max_distance_branch_removal: float = 500,
+    fill_size: float = 333,
+    file_format: str = "csv",
+    output_folder: Union[str, None] = None,
+    point_mode: bool = False,
+    radius: float = 4000,
+    water_method: str = "Jones2019",
+    start_number: int = 0,
+    maximum_number_of_tasks: int = 10,
+):
 
-    if not POINTMODE:
-        imageInfo = pd.read_csv(ID_FILE, dtype = {'LANDSAT_ID': np.unicode_})
-        sceneIDList = imageInfo['LANDSAT_ID'].values.tolist()
-    if POINTMODE:
-        imageInfo = pd.read_csv(ID_FILE, dtype = {'Point_ID': np.unicode_, 'LANDSAT_ID': np.unicode_})
-        sceneIDList = imageInfo['LANDSAT_ID'].values.tolist()
-        point_IDList = imageInfo['Point_ID'].values.tolist()
-        x = imageInfo['Longitude'].values.tolist()
-        y = imageInfo['Latitude'].values.tolist()
+    output_folder = "gee"
 
-    N = len(sceneIDList)
+    _landsat_one_image_partial = partial(
+        Rwc.one_image_river_width,
+        max_distance=max_distance,
+        max_distance_branch_removal=max_distance_branch_removal,
+        fill_size=fill_size,
+        file_format=file_format,
+        output_folder=output_folder,
+        point_mode=point_mode,
+        radius=radius,
+        water_method=water_method,
+    )
 
-    print('')
-    print('Image ID csv file is', ID_FILE)
-    print('Output file format is', FORMAT)
-    print('Results will be exported to', OUTPUT_FOLDER)
-    print('')
-    print('Number of images in the file:', N)
+    if point_mode:
+        imageInfo = pd.read_csv(
+            landsat_id_file, dtype={"Point_ID": np.unicode_, "LANDSAT_ID": np.unicode_}
+        )
+        point_IDList = list(imageInfo["Point_ID"].values)
+        x = list(imageInfo["Longitude"].values)
+        y = list(imageInfo["Latitude"].values)
 
-    for n in range(START_NO, N):
+        return _landsat_one_image_partial, zip(point_IDList, x, y)
 
-        cmdstr = 'python rwc_landsat_one_image.py ' +  sceneIDList[n] + ' -f ' + FORMAT + ' -w ' + WATER_METHOD
-        cmdstr = cmdstr + ' -d ' + str(MAXDISTANCE) + ' -i ' + str(FILL_SIZE) + ' -b ' + str(MAXDISTANCE_BRANCH_REMOVAL)
+    else:
+        imageInfo = pd.read_csv(landsat_id_file, dtype={"LANDSAT_ID": np.unicode_})
 
-        if OUTPUT_FOLDER:
-            cmdstr = cmdstr + ' -o ' + OUTPUT_FOLDER
+        sceneIDList = list(imageInfo["LANDSAT_ID"].values)
 
-        if POINTMODE:
-            cmdstr = cmdstr + ' -p -x ' + str(x[n]) + ' -y ' + str(y[n]) + ' -r ' + str(RADIUS) + ' -n ' + point_IDList[n]
+        return _landsat_one_image_partial, zip(sceneIDList)
 
-        os.system(cmdstr)
 
-        maximum_no_of_tasks(MAXIMUM_NO_OF_TASKS, 30)
+if __name__ == "__main__":
+    kwargs = _cli()
 
-        print('submitted task ', n + 1, ' of ', N)
+    partial_func, iter_object = rwc_landsat_batch(**kwargs)
+
+    with Pool(5) as pool:
+        task = pool.starmap(
+            partial(_caller, func=partial_func),
+            iter_object
+        )
+        print(task)
--- RivWidthCloud_Python/rwc_landsat_one_image.py
+++ RivWidthCloud_Python/rwc_landsat_one_image.py
@@ -1,76 +1,177 @@
-if __name__ == '__main__':
+import argparse
+import ee
+import getopt
+import numpy as np
+import sys
 
-    import ee
-    import numpy as np
-    import getopt
-    import argparse
-    import sys
-    from functions_landsat import id2Img
-    from rwc_landsat import rwGenSR
+from typing import Union
+from functools import partial
 
-    parser = argparse.ArgumentParser(prog = 'rwc_landsat_one_image.py',
-    description = "Calculate river centerline and width in the provided Landsat scene. \
-    (Example: python rwc_landsat_one_image.py LC08_L1TP_022034_20130422_20170310_01_T1 -f shp)")
+# local imports
+from functions_landsat import id2Img
+from rwc_landsat import rwGenSR
 
-    parser.add_argument('LANDSAT_ID', help = 'LANDSAT_ID for any Landsat 5, 7, and 8 SR scene', type = str)
-    parser.add_argument('-f', '--FORMAT', help = "Output file format ('csv' or 'shp'). Default: 'csv'", type = str, default = 'csv')
-    parser.add_argument('-w', '--WATER_METHOD', help = "Water classification method ('Jones2019' or 'Zou2018'). Default: 'Jones2019'", type = str, default = 'Jones2019')
-    parser.add_argument('-d', '--MAXDISTANCE', help = 'Default: 4000 meters', type = float, default = 4000)
-    parser.add_argument('-i', '--FILL_SIZE', help = 'Default: 333 pixels', type = float, default = 333)
-    parser.add_argument('-b', '--MAXDISTANCE_BRANCH_REMOVAL', help = 'Default: 500 pixels', type = float, default = 500)
-    parser.add_argument('-o', '--OUTPUT_FOLDER', help = 'Any existing folder name in Google Drive. Default: root of Google Drive', type = str, default = '')
 
-    group_validation = parser.add_argument_group(title = 'Run the RivWidthCloud in POINT mode',
-    description = 'In POINT mode, width only calculated for the region close to the point \
+def _cli() -> dict:
+    """ Cli for completing single image river width analysis """
+    parser = argparse.ArgumentParser(
+        prog="rwc_landsat_one_image.py",
+        description="Calculate river centerline and width in the provided Landsat scene. \
+    (Example: python rwc_landsat_one_image.py LC08_L1TP_022034_20130422_20170310_01_T1 -f shp)",
+    )
+
+    parser.add_argument(
+        "landsat_id", help="LANDSAT_ID for any Landsat 5, 7, and 8 SR scene", type=str
+    )
+    parser.add_argument(
+        "-f",
+        "--file_format",
+        help="Output file format ('csv' or 'shp'). Default: 'csv'",
+        type=str,
+        default="csv",
+    )
+    parser.add_argument(
+        "-w",
+        "--water_method",
+        help="Water classification method ('Jones2019' or 'Zou2018'). Default: 'Jones2019'",
+        type=str,
+        default="Jones2019",
+    )
+    parser.add_argument(
+        "-d", "--max_distance", help="Default: 4000 meters", type=float, default=4000
+    )
+    parser.add_argument(
+        "-i", "--fill_size", help="Default: 333 pixels", type=float, default=333
+    )
+    parser.add_argument(
+        "-b",
+        "--max_distance_branch_removal",
+        help="Default: 500 pixels",
+        type=float,
+        default=500,
+    )
+    parser.add_argument(
+        "-o",
+        "--output_folder",
+        help="Any existing folder name in Google Drive. Default: root of Google Drive",
+        type=str,
+        default="",
+    )
+
+    group_validation = parser.add_argument_group(
+        title="Run the RivWidthCloud in point mode",
+        description="In point mode, width only calculated for the region close to the point \
     location specified by its lon, lat, and an identifier. The radius of the region is specified through the specified buffer. \
     The point must locate within the bounds of the scene. \
-    (Example: python rwc_landsat_one_image.py LC08_L1TP_022034_20130422_20170310_01_T1 -f shp -w Zou2018 -p -x -88.263 -y 37.453 -r 2000 -n testPoint)')
+    (Example: python rwc_landsat_one_image.py LC08_L1TP_022034_20130422_20170310_01_T1 -f shp -w Zou2018 -p -x -88.263 -y 37.453 -r 2000 -n testPoint)",
+    )
 
-    group_validation.add_argument('-p', '--POINT', help = 'Enable the POINT mode', action = 'store_true')
-    group_validation.add_argument('-x', '--LONGITUDE', help = 'Longitude of the point location', type = float)
-    group_validation.add_argument('-y', '--LATITUDE', help = 'Latitude of the point location', type = float)
-    group_validation.add_argument('-r', '--BUFFER', help = 'Radius of the buffered region around the point location', type = float, default = 4000)
-    group_validation.add_argument('-n', '--POINT_NAME', help = 'identifier for the point', type = str)
+    group_validation.add_argument(
+        "-p", "--point_mode", help="Enable the point mode", action="store_true"
+    )
+    group_validation.add_argument(
+        "-x", "--lon", help="Longitude of the point location", type=float
+    )
+    group_validation.add_argument(
+        "-y", "--lat", help="Latitude of the point location", type=float
+    )
+    group_validation.add_argument(
+        "-r",
+        "--radius",
+        help="Radius of the buffered region around the point location",
+        type=float,
+        default=4000,
+    )
 
-    args = parser.parse_args()
+    return vars(parser.parse_args())
 
-    IMG_ID = args.LANDSAT_ID
-    FORMAT = args.FORMAT
-    WATER_METHOD = args.WATER_METHOD
-    MAXDISTANCE = args.MAXDISTANCE
-    FILL_SIZE = args.FILL_SIZE
-    MAXDISTANCE_BRANCH_REMOVAL = args.MAXDISTANCE_BRANCH_REMOVAL
-    OUTPUT_FOLDER = args.OUTPUT_FOLDER
+    # IMG_ID = args.LANDSAT_ID
+    # FORMAT = args.FORMAT
+    # WATER_METHOD = args.WATER_METHOD
+    # MAXDISTANCE = args.MAXDISTANCE
+    # FILL_SIZE = args.FILL_SIZE
+    # MAXDISTANCE_BRANCH_REMOVAL = args.MAXDISTANCE_BRANCH_REMOVAL
+    # OUTPUT_FOLDER = args.OUTPUT_FOLDER
 
-    POINTMODE = args.POINT
-    LONGITUDE = args.LONGITUDE
-    LATITUDE = args.LATITUDE
-    RADIUS = args.BUFFER
-    ROI_NAME = args.POINT_NAME
+    # POINTMODE = args.POINT
+    # LONGITUDE = args.LONGITUDE
+    # LATITUDE = args.LATITUDE
+    # RADIUS = args.BUFFER
+    # ROI_NAME = args.POINT_NAME
 
-    ee.Initialize()
 
-    # start of program
-    img = id2Img(IMG_ID)
+class Rwc:
+    def __init__(self, img_id):
+        ee.Initialize()
 
-    # in validation, clip the original image around the validation site
-    if POINTMODE:
-        aoi = ee.Geometry.Point([LONGITUDE, LATITUDE], "EPSG:4326").buffer(RADIUS).bounds()
-        rwc = rwGenSR(aoi = aoi, WATER_METHOD = WATER_METHOD, MAXDISTANCE = MAXDISTANCE, FILL_SIZE = FILL_SIZE, MAXDISTANCE_BRANCH_REMOVAL = MAXDISTANCE_BRANCH_REMOVAL)
-        exportPrefix = IMG_ID + '_v_' + ROI_NAME
-    else:
-        rwc = rwGenSR(WATER_METHOD = WATER_METHOD, MAXDISTANCE = MAXDISTANCE, FILL_SIZE = FILL_SIZE, MAXDISTANCE_BRANCH_REMOVAL = MAXDISTANCE_BRANCH_REMOVAL)
-        exportPrefix = IMG_ID
+        self.img_id = img_id
 
-    widthOut = rwc(img)
+    @staticmethod
+    def one_image_river_width(
+        landsat_id: str = None,
+        lon: Union[float, None] = None,
+        lat: Union[float, None] = None,
+        max_distance: float = 4000,
+        max_distance_branch_removal: float = 500,
+        fill_size: float = 333,
+        file_format: str = "csv",
+        output_folder: Union[str, None] = None,
+        point_mode: bool = False,
+        radius: float = 4000,
+        water_method: str = "Jones2019",
+    ):
+        try:
+            # Get landsat image from landsat id
+            _img = id2Img(landsat_id)
 
-    taskWidth = (ee.batch.Export.table.toDrive(
-        collection = widthOut,
-        description = exportPrefix,
-        folder = OUTPUT_FOLDER,
-        fileNamePrefix = exportPrefix,
-        fileFormat = FORMAT))
-    taskWidth.start()
+        except ee.ee_exception.EEException as e:
+            ee.Initialize()
 
-    print('')
-    print(exportPrefix, 'will be exported to', OUTPUT_FOLDER, 'as', FORMAT, 'file')
+        _img = id2Img(landsat_id)
+
+
+        aoi = (
+            (
+                ee.Geometry.Point([lon, lat], "EPSG:4326")
+                .buffer(radius)
+                .bounds()
+            )
+            if point_mode
+            else None
+        )
+
+        export_prefix = landsat_id + "_v_" if aoi else landsat_id
+
+        rwc = rwGenSR(
+            aoi=aoi,
+            WATER_METHOD=water_method,
+            MAXDISTANCE=max_distance,
+            FILL_SIZE=fill_size,
+            MAXDISTANCE_BRANCH_REMOVAL=max_distance_branch_removal,
+        )
+
+        widthOut = rwc(_img)
+        # print(widthOut.toDictionary())
+        # print(
+        #     [x for x in dir(widthOut)]
+        # )
+
+        taskWidth = ee.batch.Export.table.toDrive(
+            collection=widthOut,
+            description=export_prefix,
+            folder=output_folder,
+            fileNamePrefix=export_prefix,
+            fileFormat=file_format,
+        )
+        taskWidth.start()
+
+        print(
+            f"{export_prefix} will be exported to {output_folder} as {file_format} file"
+        )
+
+
+if __name__ == "__main__":
+    kwargs = _cli()
+    rwc  = Rwc.one_image_river_width(**kwargs)
+    # rwc = Rwc()
+    
